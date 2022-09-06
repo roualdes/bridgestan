39bb3809f4e35744573353783c283cc4614dea37
Brian Ward <bward@flatironinstitute.org>
2022-09-06 15:24:23 -0400

    Clarifying comment

--- test/test_collisions.c
+++ test/test_collisions.c
@@ -1,11 +1,20 @@
+/**
+Unlike Python or Julia, dynamic loading in R is not namespaced
+by default. All DLL objects are loaded into the global namespace,
+meaning collisions are possible if you are not careful.
+
+This exists so we can call dyn.load('./test_collisions.so')
+in R. If we were not careful (e.g. did not use the PACKAGE argument
+religiously) then this would overwrite the function exposed
+by BridgeStan.
+
+*/
+
 #include <string.h>
 
-// compile: gcc -fpic -shared -o test.so test.c
-// something like this is why we need to set PACKAGE in R
-// dyn.load('./test.so') would break all calls to name() otherwise
+const char *out = "test me";
 
-const char* out = "test me";
-
-void name_R(void** unused, char const** name_out){
+void name_R(void **unused, char const **name_out)
+{
   *name_out = out;
 }
