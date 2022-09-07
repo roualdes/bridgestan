6b3c9c443637437355ad838ac62c6a3aa0c342e9
Brian Ward <bward@flatironinstitute.org>
2022-09-07 15:50:35 -0400

    Update build_all

--- stan/build_all.sh
+++ stan/build_all.sh
@@ -4,9 +4,8 @@ cd "$BRIDGESTAN"
 models=( throw_tp throw_gq throw_lp throw_data jacobian matrix simplex full stdnormal bernoulli multi gaussian fr_gaussian simple)
 for model in "${models[@]}"
 do
-    CMDSTAN="$CMDSTAN" make -j4 O=0 stan/"$model"/"$model"_model.so &
+    CMDSTAN="$CMDSTAN" make -j2 O=0 stan/"$model"/"$model"_model.so
 done
-wait
 
 cd "$BRIDGESTAN/R/test"
 gcc -fpic -shared -o test_collisions.so test_collisions.c
