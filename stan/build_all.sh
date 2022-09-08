5022f13f59330261521b56ffcb309463fd035d67
Brian Ward <bward@flatironinstitute.org>
2022-09-08 10:53:31 -0400

    Test threading in Julia CI

--- stan/build_all.sh
+++ stan/build_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd "$BRIDGESTAN"
-models=( throw_tp throw_gq throw_lp throw_data jacobian matrix simplex full stdnormal bernoulli multi gaussian fr_gaussian simple)
+models=( throw_tp throw_gq throw_lp throw_data jacobian matrix simplex full stdnormal bernoulli gaussian fr_gaussian simple)
 for model in "${models[@]}"
 do
     CMDSTAN="$CMDSTAN" make -j2 O=0 stan/"$model"/"$model"_model.so
 done
+
+CMDSTAN="$CMDSTAN" STAN_THREADS=True make -j2 O=0 stan/multi/multi_model.so
