fc1df26a9d7bb8d21381e9e0f794f1518a055f8d
Brian Ward <bward@flatironinstitute.org>
2022-09-08 14:22:22 -0400

    Compile all models with STAN_THREADS

--- stan/build_all.sh
+++ stan/build_all.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 cd "$BRIDGESTAN"
-models=( throw_tp throw_gq throw_lp throw_data jacobian matrix simplex full stdnormal bernoulli gaussian fr_gaussian simple)
+models=( throw_tp throw_gq throw_lp throw_data jacobian matrix simplex full stdnormal bernoulli gaussian fr_gaussian simple multi )
 for model in "${models[@]}"
 do
-    CMDSTAN="$CMDSTAN" make -j2 O=0 stan/"$model"/"$model"_model.so
+    CMDSTAN="$CMDSTAN" make STAN_THREADS=True -j2 O=0 stan/"$model"/"$model"_model.so
 done
-
-CMDSTAN="$CMDSTAN" STAN_THREADS=True make -j2 O=0 stan/multi/multi_model.so
