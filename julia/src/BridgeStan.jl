fb51bf30f7e0ed0c91a2d275403e9fa0a5ad63df
Brian Ward <bward@flatironinstitute.org>
2022-09-09 11:58:23 -0400

    Add out! versions

--- julia/src/BridgeStan.jl
+++ julia/src/BridgeStan.jl
@@ -7,6 +7,11 @@ export
     param_unc_num,
     param_names,
     param_unc_names,
+    param_constrain!,
+    param_unconstrain!,
+    param_unconstrain_json!,
+    log_density_gradient!,
+    log_density_hessian!,
     param_constrain,
     param_unconstrain,
     param_unconstrain_json,
@@ -79,7 +84,6 @@ function param_names(sm::StanModel; include_tp = false, include_gq = false)
     [string(s) for s in split(unsafe_string(cstr), ',')]
 end
 
-
 function param_unc_names(sm::StanModel)
     cstr = ccall(Libc.Libdl.dlsym(sm.lib, "param_unc_names"),
                  Cstring,
@@ -88,9 +92,11 @@ function param_unc_names(sm::StanModel)
     [string(s) for s in split(unsafe_string(cstr), ',')]
 end
 
-
-function param_constrain(sm::StanModel, theta_unc; include_tp=false, include_gq=false)
-    out = zeros(param_num(sm, include_tp=include_tp, include_gq=include_gq))
+function param_constrain!(sm::StanModel, theta_unc, out::Vector{Float64}; include_tp=false, include_gq=false)
+    dims = param_num(sm; include_tp=include_tp, include_gq=include_gq)
+    if length(out) != dims
+        error("out must be same size as number of constrained parameters")
+    end
     rc = ccall(Libc.Libdl.dlsym(sm.lib, "param_constrain"),
                Cint,
                (Ptr{StanModelStruct}, Cint, Cint, Ref{Cdouble}, Ref{Cdouble}),
@@ -102,9 +108,18 @@ function param_constrain(sm::StanModel, theta_unc; include_tp=false, include_gq=
     end
 end
 
+function param_constrain(sm::StanModel, theta_unc; include_tp=false, include_gq=false)
+    out = zeros(param_num(sm, include_tp=include_tp, include_gq=include_gq))
+    param_constrain!(sm, theta_unc, out; include_tp=include_tp, include_gq=include_gq)
+end
 
-function param_unconstrain(sm::StanModel, theta)
-    out = zeros(param_unc_num(sm))
+
+function param_unconstrain!(sm::StanModel, theta, out::Vector{Float64})
+    dims = param_unc_num(sm)
+    if length(out) != dims
+        error("out must be same size as number of unconstrained parameters")
+    end
+
     rc = ccall(Libc.Libdl.dlsym(sm.lib, "param_unconstrain"),
                Cint,
                (Ptr{StanModelStruct}, Ref{Cdouble}, Ref{Cdouble}),
@@ -116,8 +131,17 @@ function param_unconstrain(sm::StanModel, theta)
     end
 end
 
-function param_unconstrain_json(sm::StanModel, theta::String)
+function param_unconstrain(sm::StanModel, theta)
     out = zeros(param_unc_num(sm))
+    param_unconstrain!(sm, theta, out)
+end
+
+function param_unconstrain_json!(sm::StanModel, theta::String, out::Vector{Float64})
+    dims = param_unc_num(sm)
+    if length(out) != dims
+        error("out must be same size as number of unconstrained parameters")
+    end
+
     rc = ccall(Libc.Libdl.dlsym(sm.lib, "param_unconstrain_json"),
                Cint,
                (Ptr{StanModelStruct}, Cstring, Ref{Cdouble}),
@@ -127,6 +151,11 @@ function param_unconstrain_json(sm::StanModel, theta::String)
     else
         out
     end
+end
+
+function param_unconstrain_json(sm::StanModel, theta::String)
+    out = zeros(param_unc_num(sm))
+    param_unconstrain_json!(sm, theta, out)
 end
 
 function log_density(sm::StanModel, q; propto = true, jacobian = true)
@@ -142,39 +171,54 @@ function log_density(sm::StanModel, q; propto = true, jacobian = true)
     end
 end
 
-function log_density_gradient(sm::StanModel, q; propto = true, jacobian = true)
+function log_density_gradient!(sm::StanModel, q, out::Vector{Float64}; propto = true, jacobian = true)
     lp = Ref{Float64}(0.0)
-    grad = zeros(param_unc_num(sm))
+    dims = param_unc_num(sm)
+    if length(out) != dims
+        error("out must be same size as number of unconstrained parameters")
+    end
 
     rc = ccall(Libc.Libdl.dlsym(sm.lib, "log_density_gradient"),
               Cint,
               (Ptr{StanModelStruct}, Cint, Cint, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-              sm.stanmodel, propto, jacobian, q, lp, grad)
+              sm.stanmodel, propto, jacobian, q, lp, out)
     if rc != 0
         error("log_density_gradient failed on C++ side; see stderr for messages")
     else
-        (lp[], grad)
+        (lp[], out)
     end
 end
 
+function log_density_gradient(sm::StanModel, q; propto = true, jacobian = true)
+    grad = zeros(param_unc_num(sm))
+    log_density_gradient!(sm, q, grad; propto=propto, jacobian=jacobian)
+end
 
-
-function log_density_hessian(sm::StanModel, q; propto = true, jacobian = true)
+function log_density_hessian!(sm::StanModel, q, out_grad::Vector{Float64}, out_hess::Vector{Float64}; propto = true, jacobian = true)
     lp = Ref{Float64}(0.0)
     dims = param_unc_num(sm)
-    grad = zeros(dims)
-    hess = zeros(dims * dims)
+    if length(out_grad) != dims
+        error("out_grad must be same size as number of unconstrained parameters")
+    elseif length(out_hess) != dims*dims
+        error("out_hess must be same size as (number of unconstrained parameters)^2")
+    end
 
     rc = ccall(Libc.Libdl.dlsym(sm.lib, "log_density_hessian"),
               Cint,
               (Ptr{StanModelStruct}, Cint, Cint, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-              sm.stanmodel, propto, jacobian, q, lp, grad, hess)
+              sm.stanmodel, propto, jacobian, q, lp, out_grad, out_hess)
     if rc != 0
         error("log_density_hessian failed on C++ side; see stderr for messages")
     else
-        (lp[], grad, reshape(hess, (dims, dims)))
+        (lp[], out_grad, reshape(out_hess, (dims, dims)))
     end
 end
 
+function log_density_hessian(sm::StanModel, q; propto = true, jacobian = true)
+    dims = param_unc_num(sm)
+    grad = zeros(dims)
+    hess = zeros(dims * dims)
+    log_density_hessian!(sm,q, grad, hess; propto=propto, jacobian=jacobian)
+end
 
 end
