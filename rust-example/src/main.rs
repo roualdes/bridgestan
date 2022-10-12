d8fe1c687d9e6c2af15b3e6d791d859f9c4e4b10
Brian Ward <bward@flatironinstitute.org>
2022-10-12 16:08:17 -0400

    Error handling tweak

--- rust-example/src/main.rs
+++ rust-example/src/main.rs
@@ -1,10 +1,11 @@
 pub mod bs_safe;
 pub mod bs_unsafe;
 
-use bs_safe::{BridgeStanError, StanModel};
+use bs_safe::StanModel;
 use std::env;
+use std::error::Error;
 
-fn main() -> Result<(), BridgeStanError> {
+fn main() -> Result<(), Box<dyn Error>> {
     let data_path = env::args().nth(1).unwrap_or("".to_string());
 
     let model = StanModel::new(&data_path, 123, 0)?;
