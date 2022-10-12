6053ba64c766ca454a9860ebfcf0eb7d094177e3
Brian Ward <bward@flatironinstitute.org>
2022-10-12 15:39:58 -0400

    Reorganize

---
+++ rust-example/src/bs_unsafe.rs
@@ -0,0 +1,3 @@
+#![allow(non_camel_case_types)]
+// Include generated bindings file
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- rust-example/src/main.rs
+++ rust-example/src/main.rs
@@ -1,68 +1,20 @@
-#![allow(non_camel_case_types)]
-pub mod bs {
-    // Include generated bindings file
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
+pub mod bs_safe;
+pub mod bs_unsafe;
 
+use bs_safe::{BridgeStanError, StanModel};
 use std::env;
-use std::ffi::CStr;
-use std::ffi::CString;
-use std::str::Utf8Error;
 
-/// Safe wrapper for BridgeStan C functions
-#[non_exhaustive]
-pub struct StanModel {
-    model: *mut bs::model_rng,
-}
-
-impl StanModel {
-    pub fn new(path: &str, seed: u32, chain_id: u32) -> Result<Self, String> {
-        let data = match CString::new(path) {
-            Ok(string) => string.into_raw(),
-            Err(_) => return Err("Failed to convert data path to String".to_string()),
-        };
-
-        let model = unsafe { bs::construct(data, seed, chain_id) };
-
-        // retake pointer to free memory
-        let _ = unsafe { CString::from_raw(data) };
-
-        if model.is_null() {
-            return Err("Failed to allocate model_rng".to_string());
-        }
-        Ok(StanModel { model })
-    }
-
-    pub fn name(&self) -> Result<&str, Utf8Error> {
-        let cstr = unsafe { CStr::from_ptr(bs::name(self.model)) };
-        cstr.to_str()
-    }
-
-    pub fn param_num(&self, include_tp: bool, include_gq: bool) -> i32 {
-        unsafe { bs::param_num(self.model, include_tp as i32, include_gq as i32) }
-    }
-
-    // etc, need more functions
-    // using vec.as_mut_ptr(), etc
-}
-
-impl Drop for StanModel {
-    fn drop(&mut self) {
-        if unsafe { bs::destruct(self.model) } != 0 {
-            panic!("Deallocating model_rng failed")
-        }
-    }
-}
-
-fn main() {
+fn main() -> Result<(), BridgeStanError> {
     let data_path = env::args().nth(1).unwrap_or("".to_string());
 
-    let model = StanModel::new(&data_path, 123, 0).unwrap();
+    let model = StanModel::new(&data_path, 123, 0)?;
 
-    let s = model.name().unwrap();
+    let s = model.name()?;
     println!("The model's name is {}.", s);
     println!(
         "The model has {} parameters.",
         model.param_num(false, false)
     );
+
+    Ok(())
 }
