365804b3e9e51d4056441fd5f14ae452489d94d0
Brian Ward <bward@flatironinstitute.org>
2022-12-01 16:23:51 -0500

    Update tests

--- python/test/test_stanmodel.py
+++ python/test/test_stanmodel.py
@@ -637,7 +637,7 @@ def recompile_simple():
     stanfile = STAN_FOLDER / "simple" / "simple.stan"
     lib = bs.compile.generate_so_name(stanfile)
     lib.unlink(missing_ok=True)
-    res = bs.compile_model(stanfile, ["BRIDGESTAN_AD_HESSIAN=true"])
+    res = bs.compile_model(stanfile, make_args=["BRIDGESTAN_AD_HESSIAN=true"])
 
     yield res
 
