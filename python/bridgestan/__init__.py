6e5a83edfab35a1db5a2edd82ff1325a6e0c7658
Brian Ward <bward@flatironinstitute.org>
2023-05-08 09:19:50 -0400

    Look for more options for mingw dlls

--- python/bridgestan/__init__.py
+++ python/bridgestan/__init__.py
@@ -29,7 +29,7 @@ if _plt.system() == "Windows":
 
         try:
             out = subprocess.run(
-                ["where.exe", "libwinpthread-1.dll"], check=True, capture_output=True
+                ["where.exe", "libwinpthread-1.dll", "libgcc_s_seh-1.dll", "libstdc++-6.dll"], check=True, capture_output=True
             )
             mingw_dir = os.path.dirname(out.stdout.decode().splitlines()[0])
             os.add_dll_directory(mingw_dir)
