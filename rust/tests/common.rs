1f3ff801a1a62b34274912d07414912c76076e2b
Brian Ward <bward@flatironinstitute.org>
2023-05-23 15:20:13 -0400

    Split up tests

---
+++ rust/tests/common.rs
@@ -0,0 +1,35 @@
+
+use std::{
+    ffi::CString,
+    path::{Path, PathBuf},
+};
+
+use bridgestan::{open_library, StanLibrary};
+
+pub fn model_dir() -> PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("test_models")
+}
+
+/// Load stan library and corresponding data if available
+pub fn get_model<S: AsRef<str>>(name: S) -> (StanLibrary, Option<CString>) {
+    let name = name.as_ref();
+    let mut base = model_dir();
+    base.push(name);
+    let lib = base.join(format!("{}_model.so", name));
+    if !lib.exists() {
+        panic!("Could not find compiled model {}", name);
+    }
+    let lib = open_library(lib).unwrap();
+
+    let data_path = base.join(name).with_extension("data.json");
+
+    if data_path.exists() {
+        let contents = std::fs::read(data_path).unwrap();
+        (lib, Some(CString::new(contents).unwrap()))
+    } else {
+        (lib, None)
+    }
+}
--- rust/tests/loading.rs
+++ rust/tests/loading.rs
@@ -1,41 +1,12 @@
+mod common;
+use common::{get_model, model_dir};
+
 use std::{
-    f64::consts::PI,
-    ffi::CString,
-    path::{Path, PathBuf},
     sync::mpsc::{sync_channel, Receiver, SyncSender},
     thread::spawn,
 };
 
-use approx::{assert_abs_diff_eq, assert_ulps_eq};
-use bridgestan::{open_library, BridgeStanError, Model, StanLibrary};
-
-fn model_dir() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR"))
-        .parent()
-        .unwrap()
-        .join("test_models")
-}
-
-/// Load stan library and corresponding data if available
-fn get_model<S: AsRef<str>>(name: S) -> (StanLibrary, Option<CString>) {
-    let name = name.as_ref();
-    let mut base = model_dir();
-    base.push(name);
-    let lib = base.join(format!("{}_model.so", name));
-    if !lib.exists() {
-        panic!("Could not find compiled model {}", name);
-    }
-    let lib = open_library(lib).unwrap();
-
-    let data_path = base.join(name).with_extension("data.json");
-
-    if data_path.exists() {
-        let contents = std::fs::read(data_path).unwrap();
-        (lib, Some(CString::new(contents).unwrap()))
-    } else {
-        (lib, None)
-    }
-}
+use bridgestan::Model;
 
 #[test]
 fn create_all_serial() {
@@ -231,7 +202,6 @@ fn load_order_all_serial() {
 
 // Test for bug https://github.com/roualdes/bridgestan/issues/111
 #[test]
-#[ignore]
 fn load_order_min_parallel() {
     let names = ["bernoulli", "gaussian", "jacobian"];
     let (senders, handles): (Vec<_>, Vec<_>) = names
@@ -313,111 +283,4 @@ fn load_parallel() {
     handles
         .into_iter()
         .for_each(|handle| handle.join().unwrap())
-}
-
-#[test]
-fn throw_data() {
-    let (lib, data) = get_model("throw_data");
-    let Err(err) = Model::new(&lib, data, 42) else {
-        panic!("throw_data model should not successfully be created.");
-    };
-
-    let BridgeStanError::ConstructFailed(msg) = err else {
-        panic!("Creating throw_data model return an unexpected error");
-    };
-    assert!(msg.contains("find this text: datafails"));
-}
-
-#[test]
-#[should_panic(expected = "number of parameters")]
-fn bad_arglength() {
-    let (lib, data) = get_model("stdnormal");
-    let model = Model::new(&lib, data, 42).unwrap();
-    let theta = vec![];
-    let mut grad = vec![];
-    let _ = model.log_density_gradient(&theta[..], true, true, &mut grad[..]);
-}
-
-#[test]
-fn logp_gradient() {
-    let (lib, data) = get_model("stdnormal");
-    let model = Model::new(&lib, data, 42).unwrap();
-    let theta = vec![1f64];
-    let mut grad = vec![0f64];
-    let logp = model
-        .log_density_gradient(&theta[..], false, true, &mut grad[..])
-        .unwrap();
-    assert_ulps_eq!(logp, (2. * PI).sqrt().recip().ln() - 0.5);
-    assert_ulps_eq!(grad[0], -1f64);
-}
-
-#[test]
-fn logp_hessian() {
-    let (lib, data) = get_model("stdnormal");
-    let model = Model::new(&lib, data, 42).unwrap();
-    let theta = vec![1f64];
-    let mut grad = vec![0f64];
-    let mut hessian = vec![0f64];
-    let logp = model
-        .log_density_hessian(&theta[..], false, true, &mut grad[..], &mut hessian)
-        .unwrap();
-    assert_ulps_eq!(logp, (2. * PI).sqrt().recip().ln() - 0.5);
-    assert_ulps_eq!(grad[0], -1f64);
-    assert_abs_diff_eq!(hessian[0], -1f64, epsilon = 1e-10);
-}
-
-#[test]
-fn test_params() {
-    let (lib, data) = get_model("bernoulli");
-    let model = Model::new(&lib, data, 42).unwrap();
-
-    let n = model.param_num(false, false);
-    assert!(n == 1);
-    let k = model.param_num(true, false);
-    let m = model.param_num(false, true);
-    let l = model.param_num(true, true);
-    assert!(k == 2);
-    assert!(m == 2);
-    assert!(l == 3);
-
-    assert_eq!(model.param_names(true, true), "theta,logit_theta,y_sim");
-
-    let mut theta_unc = vec![100f64];
-    let mut out1 = vec![-1f64];
-    let mut out2 = vec![-1f64; 2];
-    let mut out3 = vec![-1f64; 3];
-
-    let mut rng = model.new_rng(0).unwrap();
-
-    model
-        .param_constrain(&theta_unc[..], true, true, &mut out3, Some(&mut rng))
-        .unwrap();
-    assert!((out3[2] == 1.) | (out3[2] == 0.));
-
-    assert!(out3[0] == 1.);
-    assert_eq!(out3[1], f64::INFINITY);
-
-    model
-        .param_constrain(&theta_unc[..], true, false, &mut out2, Some(&mut rng))
-        .unwrap();
-    assert_eq!(out2, [1., f64::INFINITY]);
-
-    model
-        .param_constrain(&theta_unc[..], false, true, &mut out2, Some(&mut rng))
-        .unwrap();
-    assert_eq!(out2[0], 1.);
-    assert!((out2[0] == 1.) | (out2[0] == 0.));
-
-    model
-        .param_constrain(&theta_unc[..], false, false, &mut out1, Some(&mut rng))
-        .unwrap();
-    assert_eq!(out1[0], 1.);
-
-    model.param_unconstrain(&out1, &mut theta_unc).unwrap();
-    assert_eq!(theta_unc[0], f64::INFINITY);
-
-    model
-        .param_unconstrain_json(CString::new(r#"{"theta": 0.5}"#).unwrap(), &mut theta_unc)
-        .unwrap();
-    assert_eq!(theta_unc[0], 0.);
 }
