09bd7450bb4eae33d8d7f84ef767358c3cd5def1
Brian Ward <bward@flatironinstitute.org>
2025-01-02 10:20:40 -0500

    Reapply "Bump min Julia version to new LTS (1.10) (#257)"
    This reverts commit 9e299c8590dee182630c74a361bd7df014b64d75.

--- julia/test/util_tests.jl
+++ julia/test/util_tests.jl
@@ -1,30 +1,5 @@
 using BridgeStan
-using BridgeStan: @const
 using Test
-
-mutable struct Foo
-    x::Any
-    @const y
-end
-
-@testset "@const utility" begin
-    a = Foo(1, 2)
-    @test a.x == 1
-    @test a.y == 2
-    a.x = 3
-    @test a.x == 3
-    @test a.y == 2
-    if VERSION ≥ v"1.8"
-        # y is const
-        @test_throws ErrorException a.y = 4
-    else
-        # y is mutable
-        a.y = 4
-        @test a.y == 4
-    end
-end
-
-
 
 @testset "download" begin
     withenv("BRIDGESTAN" => nothing) do
