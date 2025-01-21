151765014566673849ba957d9303eb59df8fb3ee
Brian Ward <bward@flatironinstitute.org>
2025-01-21 15:35:05 -0400

    Cleanup R tests (#263)

--- R/tests/testthat/setup.R
+++ R/tests/testthat/setup.R
@@ -1,15 +1,25 @@
 base = "../../.."
 
+loaded <- c()
+
 load_model <- function(name, include_data = TRUE) {
     if (include_data) {
-        data = file.path(base, "test_models", name, paste0(name, ".data.json"))
+        data <- file.path(base, "test_models", name, paste0(name, ".data.json"))
     } else {
-        data = ""
+        data <- ""
     }
-    model <- StanModel$new(file.path(base, "test_models", name, paste0(name, "_model.so")),
-        data, 1234)
+
+    file <- file.path(base, "test_models", name, paste0(name, ".stan"))
+
+    if (name %in% loaded) {
+        expect_warning(
+            model <- StanModel$new(file, data, 1234),
+            ".*which is already loaded.*")
+
+     } else {
+        loaded <<- c(loaded, name)
+        model <- StanModel$new(file, data, 1234)
+     }
+
     return(model)
 }
-
-simple <- load_model("simple")
-bernoulli <- load_model("bernoulli")
